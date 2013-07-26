0486d0f510d7fdfc65826dcb50d725af43f0bd76
Sumeet Jain <sumeet@sumeetjain.com>
2013-07-25 21:43:45 -0700

    Don't refresh table unless there's a table

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
-  var refresh_stocks_table = function(){
-    $.ajax({
-      dataType: "script",
-      type: "get",
-      url: "/users/refresh_table"
-    });
-  };
+  if ($("#stocks").length) {
+    var refresh_stocks_table = function(){
+      $.ajax({
+        dataType: "script",
+        type: "get",
+        url: "/users/refresh_table"
+      });
+    };
 
-  // $("#refresh_stocks").on('click', refresh_stocks_table);
+    // $("#refresh_stocks").on('click', refresh_stocks_table);
 
-  setInterval(refresh_stocks_table, 1000);
+    setInterval(refresh_stocks_table, 1000);
+  }
 });
